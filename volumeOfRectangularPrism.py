d082a97c5a855467dc9ac160db04925dda44d74e
Kyanh Pham <kyanh.pham@mths.ca>
2022-10-06 14:15:11 +0000

    calculates volume of rectangular prism

---
+++ volumeOfRectangularPrism.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Kyanh Pham
+# Created on: Oct 2022
+# This program calculates the volume of a rectangular prism
+# with length, width, and height inputted from user
+
+
+def main():
+    # This function calculates volume
+    print("This will calculate the volume of a rectangular prism or cube.")
+
+    # Input
+    length = int(input("Enter the length (cm): "))
+    width = int(input("Enter the width (cm): "))
+    height = int(input("Enter the height (cm): "))
+
+    # Process
+    volume = length * width * height
+
+    # Output
+    print("Volume is {0} cm³.".format(volume))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
